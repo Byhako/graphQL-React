a0d62669c7a8559f7855941fd5f6be307d30ade8
Ruben E Acosta <eldragondera1@gmail.com>
2020-03-01 18:01:08 -0500

    using useMutation

--- client/src/components/EditClient.tsx
+++ client/src/components/EditClient.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { CLIENT_QUERY } from '../queries';
 
 import { NEW_CLIENT } from '../mutations';
-import { Mutation } from 'react-apollo';
 
 const EditClient = (props) => {
   const id: number = props.match.params.id;
@@ -58,6 +57,11 @@ const EditClient = (props) => {
     }
   }, [error]);
 
+  const [createClient] = useMutation(NEW_CLIENT,{
+    onCompleted: () => setMessageModal(`Cliente ${state.name} ${state.surname} creado.`),
+    onError: (error) => {console.log(error); setMessageModal('Error. Cliente no creado.')}
+  });
+
   const handleChangeEmails = (e: any, idx: number) => {
     const value: string = e.target.value;
     const emails: object[] = state.emails.map((email: object) => email);
@@ -69,7 +73,7 @@ const EditClient = (props) => {
     setState({ ...state, emails });
   };
 
-  const handleCreateClient = (e, createClient) => {
+  const handleUpdateClient = (e, createClient) => {
     e.preventDefault();
     let emailsError: boolean = false;
 
@@ -128,124 +132,116 @@ const EditClient = (props) => {
       {err && (
         <h5 className='alert alert-danger p-3 w-100 text-center'>Faltan campos por llenar!</h5>
       )}
-      <Mutation
-        mutation={NEW_CLIENT}
-        onCompleted={() => setMessageModal(`Cliente ${state.name} ${state.surname} actualizado.`)}
-        onError={() => setMessageModal('Error. Cliente no actualizado.')}
+      <form
+        className='col-8 m-3'
+        onSubmit={e => handleUpdateClient(e, createClient)}
       >
-        {createClient => (
-          <form
-            className='col-8 m-3'
-            onSubmit={e => handleCreateClient(e, createClient)}
-          >
-            <div className="form-row">
-              <div className="form-group col-6">
-                <label htmlFor="name">Nombre</label>
-                <input
-                  type="text"
-                  id='name'
-                  required
-                  className='form-control'
-                  placeholder='Nombre'
-                  value={state.name}
-                  onChange={e => setState({ ...state, name: e.target.value })}
-                />
-              </div>
-              <div className="form-group col-6">
-                <label htmlFor="surname">Apellido</label>
-                <input
-                  type="text"
-                  id='surname'
-                  required
-                  className='form-control'
-                  placeholder='Apellido'
-                  value={state.surname}
-                  onChange={e => setState({ ...state, surname: e.target.value })}
-                />
-              </div>
+        <div className="form-row">
+          <div className="form-group col-6">
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              id='name'
+              required
+              className='form-control'
+              placeholder='Nombre'
+              value={state.name}
+              onChange={e => setState({ ...state, name: e.target.value })}
+            />
+          </div>
+          <div className="form-group col-6">
+            <label htmlFor="surname">Apellido</label>
+            <input
+              type="text"
+              id='surname'
+              required
+              className='form-control'
+              placeholder='Apellido'
+              value={state.surname}
+              onChange={e => setState({ ...state, surname: e.target.value })}
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-12">
+            <label htmlFor="company">Empresa</label>
+            <input
+              type="text"
+              id='company'
+              required
+              className='form-control'
+              placeholder='Empresa'
+              value={state.company}
+              onChange={e => setState({ ...state, company: e.target.value })}
+            />
+          </div>
+        </div>
+        
+        <div className="form-row d-flex justify-content-center mb-4">
+          {state.emails.map((email: Email, idx: number) => (
+            <div className="form-group col-12" key={idx}>
+              <label htmlFor={`email${idx}`}>
+                Email {numberEmails > 1 ? `${idx + 1}` : ''}
+              </label>
+              <input
+                type="email"
+                id={`email${idx}`}
+                required
+                className='form-control'
+                placeholder='Email'
+                value={email.email}
+                onChange={e => handleChangeEmails(e, idx)}
+              />
+              {numberEmails > 1 && (
+                <span
+                  style={{
+                    fontSize: '24px',
+                    position: 'absolute',
+                    right: '12px',
+                    top: '28px',
+                    color: 'red',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => handleRemoveEmail(idx)}
+                >&times;</span>
+              )}
             </div>
-            <div className="form-row">
-              <div className="form-group col-12">
-                <label htmlFor="company">Empresa</label>
-                <input
-                  type="text"
-                  id='company'
-                  required
-                  className='form-control'
-                  placeholder='Empresa'
-                  value={state.company}
-                  onChange={e => setState({ ...state, company: e.target.value })}
-                />
-              </div>
-            </div>
-            
-            <div className="form-row d-flex justify-content-center mb-4">
-              {state.emails.map((email: Email, idx: number) => (
-                <div className="form-group col-12" key={idx}>
-                  <label htmlFor={`email${idx}`}>
-                    Email {numberEmails > 1 ? `${idx + 1}` : ''}
-                  </label>
-                  <input
-                    type="email"
-                    id={`email${idx}`}
-                    required
-                    className='form-control'
-                    placeholder='Email'
-                    value={email.email}
-                    onChange={e => handleChangeEmails(e, idx)}
-                  />
-                  {numberEmails > 1 && (
-                    <span
-                      style={{
-                        fontSize: '24px',
-                        position: 'absolute',
-                        right: '12px',
-                        top: '28px',
-                        color: 'red',
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => handleRemoveEmail(idx)}
-                    >&times;</span>
-                  )}
-                </div>
-              ))}
-              <button
-                type='button'
-                className='btn btn-warning float-right'
-                onClick={handleAddEmail}
-              >Agregar Email</button>
-            </div>
+          ))}
+          <button
+            type='button'
+            className='btn btn-warning float-right'
+            onClick={handleAddEmail}
+          >Agregar Email</button>
+        </div>
 
-            <div className="form-row">
-              <div className="form-group col-6">
-                <label htmlFor="age">Edad</label>
-                <input
-                  type="text"
-                  id='age'
-                  required
-                  className='form-control'
-                  placeholder='Edad'
-                  value={state.age}
-                  onChange={e => setState({ ...state, age: +e.target.value })}
-                />
-              </div>
-              <div className="form-group col-6">
-                <label htmlFor="type">Tipo de Cliente</label>
-                <select
-                  className='form-control'
-                  value={state.type}
-                  onChange={e => setState({ ...state, type: e.target.value })}
-                >
-                  <option value="">Elegir...</option>
-                  <option value="BASIC">BASIC</option>
-                  <option value="PREMIUM">PREMIUM</option>
-                </select>
-              </div>
-            </div>
-            <button type='submit' className='btn btn-success float-right'>Guardar Cambios</button>
-          </form>
-        )}
-      </Mutation>
+        <div className="form-row">
+          <div className="form-group col-6">
+            <label htmlFor="age">Edad</label>
+            <input
+              type="text"
+              id='age'
+              required
+              className='form-control'
+              placeholder='Edad'
+              value={state.age}
+              onChange={e => setState({ ...state, age: +e.target.value })}
+            />
+          </div>
+          <div className="form-group col-6">
+            <label htmlFor="type">Tipo de Cliente</label>
+            <select
+              className='form-control'
+              value={state.type}
+              onChange={e => setState({ ...state, type: e.target.value })}
+            >
+              <option value="">Elegir...</option>
+              <option value="BASIC">BASIC</option>
+              <option value="PREMIUM">PREMIUM</option>
+            </select>
+          </div>
+        </div>
+        <button type='submit' className='btn btn-success float-right'>Guardar Cambios</button>
+      </form>
 
       {/* Modal */}
       <div
--- client/src/components/NewClient.tsx
+++ client/src/components/NewClient.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useMutation } from '@apollo/react-hooks';
 import { NEW_CLIENT } from '../mutations';
-import { Mutation } from 'react-apollo';
 
 const NewClient = () => {
   interface Email { email: string };
@@ -26,6 +26,11 @@ const NewClient = () => {
 
   const [error, setError] = useState<boolean>(false);
   const [messageModal, setMessageModal] = useState<string>('');
+
+  const [createClient] = useMutation(NEW_CLIENT,{
+    onCompleted: () => setMessageModal(`Cliente ${state.name} ${state.surname} creado.`),
+    onError: (error) => {console.log(error); setMessageModal('Error. Cliente no creado.')}
+  });
 
   useEffect(() => {
     if (error) {
@@ -102,124 +107,117 @@ const NewClient = () => {
       {error && (
         <h5 className='alert alert-danger p-3 w-100 text-center'>Faltan campos por llenar!</h5>
       )}
-      <Mutation
-        mutation={NEW_CLIENT}
-        onCompleted={() => setMessageModal(`Cliente ${state.name} ${state.surname} creado.`)}
-        onError={() => setMessageModal('Error. Cliente no creado.')}
-      >
-        {createClient => (
-          <form
-            className='col-8 m-3'
-            onSubmit={e => handleCreateClient(e, createClient)}
-          >
-            <div className="form-row">
-              <div className="form-group col-6">
-                <label htmlFor="name">Nombre</label>
-                <input
-                  type="text"
-                  id='name'
-                  required
-                  className='form-control'
-                  placeholder='Nombre'
-                  value={state.name}
-                  onChange={e => setState({ ...state, name: e.target.value })}
-                />
-              </div>
-              <div className="form-group col-6">
-                <label htmlFor="surname">Apellido</label>
-                <input
-                  type="text"
-                  id='surname'
-                  required
-                  className='form-control'
-                  placeholder='Apellido'
-                  value={state.surname}
-                  onChange={e => setState({ ...state, surname: e.target.value })}
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group col-12">
-                <label htmlFor="company">Empresa</label>
-                <input
-                  type="text"
-                  id='company'
-                  required
-                  className='form-control'
-                  placeholder='Empresa'
-                  value={state.company}
-                  onChange={e => setState({ ...state, company: e.target.value })}
-                />
-              </div>
-            </div>
-            
-            <div className="form-row d-flex justify-content-center mb-4">
-              {state.emails.map((email: Email, idx: number) => (
-                <div className="form-group col-12" key={idx}>
-                  <label htmlFor={`email${idx}`}>
-                    Email {numberEmails > 1 ? `${idx + 1}` : ''}
-                  </label>
-                  <input
-                    type="email"
-                    id={`email${idx}`}
-                    required
-                    className='form-control'
-                    placeholder='Email'
-                    value={email.email}
-                    onChange={e => handleChangeEmails(e, idx)}
-                  />
-                  {numberEmails > 1 && (
-                    <span
-                      style={{
-                        fontSize: '24px',
-                        position: 'absolute',
-                        right: '12px',
-                        top: '28px',
-                        color: 'red',
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => handleRemoveEmail(idx)}
-                    >&times;</span>
-                  )}
-                </div>
-              ))}
-              <button
-                type='button'
-                className='btn btn-warning float-right'
-                onClick={handleAddEmail}
-              >Agregar Email</button>
-            </div>
 
-            <div className="form-row">
-              <div className="form-group col-6">
-                <label htmlFor="age">Edad</label>
-                <input
-                  type="text"
-                  id='age'
-                  required
-                  className='form-control'
-                  placeholder='Edad'
-                  value={state.age}
-                  onChange={e => setState({ ...state, age: +e.target.value })}
-                />
-              </div>
-              <div className="form-group col-6">
-                <label htmlFor="type">Tipo de Cliente</label>
-                <select
-                  className='form-control'
-                  value={state.type}
-                  onChange={e => setState({ ...state, type: e.target.value })}
-                >
-                  <option value="">Elegir...</option>
-                  <option value="BASIC">BASIC</option>
-                  <option value="PREMIUM">PREMIUM</option>
-                </select>
-              </div>
+      <form
+        className='col-8 m-3'
+        onSubmit={e => handleCreateClient(e, createClient)}
+      >
+        <div className="form-row">
+          <div className="form-group col-6">
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              id='name'
+              required
+              className='form-control'
+              placeholder='Nombre'
+              value={state.name}
+              onChange={e => setState({ ...state, name: e.target.value })}
+            />
+          </div>
+          <div className="form-group col-6">
+            <label htmlFor="surname">Apellido</label>
+            <input
+              type="text"
+              id='surname'
+              required
+              className='form-control'
+              placeholder='Apellido'
+              value={state.surname}
+              onChange={e => setState({ ...state, surname: e.target.value })}
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-12">
+            <label htmlFor="company">Empresa</label>
+            <input
+              type="text"
+              id='company'
+              required
+              className='form-control'
+              placeholder='Empresa'
+              value={state.company}
+              onChange={e => setState({ ...state, company: e.target.value })}
+            />
+          </div>
+        </div>
+        
+        <div className="form-row d-flex justify-content-center mb-4">
+          {state.emails.map((email: Email, idx: number) => (
+            <div className="form-group col-12" key={idx}>
+              <label htmlFor={`email${idx}`}>
+                Email {numberEmails > 1 ? `${idx + 1}` : ''}
+              </label>
+              <input
+                type="email"
+                id={`email${idx}`}
+                required
+                className='form-control'
+                placeholder='Email'
+                value={email.email}
+                onChange={e => handleChangeEmails(e, idx)}
+              />
+              {numberEmails > 1 && (
+                <span
+                  style={{
+                    fontSize: '24px',
+                    position: 'absolute',
+                    right: '12px',
+                    top: '28px',
+                    color: 'red',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => handleRemoveEmail(idx)}
+                >&times;</span>
+              )}
             </div>
-            <button type='submit' className='btn btn-success float-right'>Guardar Cambios</button>
-          </form>
-        )}
-      </Mutation>
+          ))}
+          <button
+            type='button'
+            className='btn btn-warning float-right'
+            onClick={handleAddEmail}
+          >Agregar Email</button>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group col-6">
+            <label htmlFor="age">Edad</label>
+            <input
+              type="text"
+              id='age'
+              required
+              className='form-control'
+              placeholder='Edad'
+              value={state.age}
+              onChange={e => setState({ ...state, age: +e.target.value })}
+            />
+          </div>
+          <div className="form-group col-6">
+            <label htmlFor="type">Tipo de Cliente</label>
+            <select
+              className='form-control'
+              value={state.type}
+              onChange={e => setState({ ...state, type: e.target.value })}
+            >
+              <option value="">Elegir...</option>
+              <option value="BASIC">BASIC</option>
+              <option value="PREMIUM">PREMIUM</option>
+            </select>
+          </div>
+        </div>
+        <button type='submit' className='btn btn-success float-right'>Guardar Cambios</button>
+      </form>
 
       {/* Modal */}
       <div
